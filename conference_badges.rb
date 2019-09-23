ee3ac29afa8e235e24fd071a1900ebf2c6284fe3
Charlie Oster <oster.charlie@yahoo.com>
2019-09-23 23:16:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,17 @@ def batch_badge_creator(attendees)
     badge_maker(attendee)
   
   end
+
 end
 
 def assign_rooms (attendees)
+attendees.map.with_index do |attendee, index |
+  "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+ end
+
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|badge| puts badge}
+  
 end
